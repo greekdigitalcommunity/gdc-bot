83b8c459f288ee6309f08d977a553a752945ab17
thanosIrodotou <iraikky@gmail.com>
2018-04-12 19:44:26 +0100

    partial admin bot port to botkit from hubot

---
+++ skills/stats.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const stats = {
+    slashCommands: 0,
+    triggers: 0,
+    convos: 0
+};
+
+const presentStats = (bot, message) => {
+  bot.api.users.list({presence: true} , (err, response) => {
+    let users = 0;
+    let active = 0;
+    response.members.forEach(user => {
+      if (!user.is_bot && !user.is_app_user && !user.deleted) {
+        if (user.presence === 'active') {
+          active++;
+        }
+        users++;
+      }
+    });
+    bot.replyPrivate(message,
+      `*${users}* registered - *${active}* active\n`
+       + `*${stats.slashCommands}* slashCommandActions\n`
+       + `*${stats.triggers}* triggerActions\n`
+       + `*${stats.convos}* conversationStartedActions\n`
+    );
+  });
+};
+
+module.exports = function(controller) {
+
+  controller.on('heard_trigger', function() {
+      stats.triggers++;
+  });
+
+  controller.on('conversationStarted', function() {
+      stats.convos++;
+  });
+
+  controller.on('slash_command', function(bot, message) {
+    stats.slashCommands++;
+
+    bot.api.users.info({user: message.user_id}, (err, response) => {
+      if (response.user.is_admin) {
+        presentStats(bot, message);
+      }
+    });
+  });
+};
