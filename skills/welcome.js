a20ff090c47620461bf22653c631923fdc8a967e
Thanos Korakas <thanoskorakas@gmail.com>
2018-05-01 22:30:01 +0300

    Added welcome skill and required packages

---
+++ skills/welcome.js
@@ -0,0 +1,99 @@
+'use strict';
+
+module.exports = function (controller) {
+
+  controller.hears(['form'], 'direct_message,direct_mention', function (bot, message) {
+    bot.reply(message, {
+      attachments: [
+        {
+          title: 'Do you want to interact with my buttons?',
+          callback_id: '123',
+          attachment_type: 'default',
+          actions: [
+            {
+              'name': 'yes',
+              'text': 'Yes',
+              'value': 'yes',
+              'type': 'button',
+            },
+            {
+              'name': 'no',
+              'text': 'No',
+              'value': 'no',
+              'type': 'button',
+            }
+          ]
+        }
+      ]
+    });
+  });
+
+  controller.on('member_joined_channel', function (bot, message) {
+
+    bot.api.channels.info({channel: message.channel}, function (err, response) {
+      console.log(response);
+      if (response.channel.name === 'hello') {
+        bot.startConversation(message, function (err, convo) {
+          convo.say('Better take this private...');
+          convo.say({
+            ephemeral: true, attachments: [
+              {
+                title: 'Do you want to interact with my buttons?',
+                callback_id: '123',
+                attachment_type: 'default',
+                actions: [
+                  {
+                    'name': 'yes',
+                    'text': 'Yes',
+                    'value': 'yes',
+                    'type': 'button',
+                  },
+                  {
+                    'name': 'no',
+                    'text': 'No',
+                    'value': 'no',
+                    'type': 'button',
+                  }
+                ]
+              }
+            ]
+          });
+        });
+      };
+    });
+
+  });
+
+  controller.on('interactive_message_callback', function (bot, message) {
+    console.log(message);
+    var dialog = bot.createDialog(
+      'GDC Bot',
+      '133',
+      'Submit'
+    ).addText('Text', 'text', 'some text')
+      .addSelect('Select', 'select', null, [{label: 'Foo', value: 'foo'}, {
+        label: 'Bar',
+        value: 'bar'
+      }], {placeholder: 'Select One'})
+      .addTextarea('Textarea', 'textarea', 'some longer text', {placeholder: 'Put words here'})
+      .addUrl('Website', 'url', 'http://greekdigitalcommunity.com');
+
+    bot.replyWithDialog(message, dialog.asObject());
+    // console.log(message)
+    // bot.replyInteractive(message, 'Thank you');
+  });
+
+  controller.on('dialog_submission', function handler (bot, message) {
+    console.log(message);
+    // bot.replyPrivate(message, 'Got it!');
+    bot.sendEphemeral({
+      channel: 'hello',
+      user: message.user,
+      replace_original: true,
+      text: 'Pssst! You my friend, are a true Bot Champion!'
+    });
+
+    // call dialogOk or else Slack will think this is an error
+    bot.dialogOk();
+  });
+};
