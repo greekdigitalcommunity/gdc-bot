5edd1715d94cf5a58dde2f1b7e46e3aee73047f8
Thanos Korakas <thanoskorakas@gmail.com>
2018-05-07 22:17:59 +0300

    Added absolute path ffs

--- scripts/noob_fs.js
+++ scripts/noob_fs.js
@@ -8,8 +8,7 @@ const setupTeamFile = () => {
   try {
     let parsedJson = JSON.parse(NOOB_FS);
     console.log('loaded team info', parsedJson);
-    fs.closeSync(fs.openSync(`.data/db/teams/${parsedJson.id}.json`, 'a'));
-    fs.writeFileSync(`.data/db/teams/${parsedJson.id}.json`, JSON.stringify(parsedJson));
+    fs.writeFileSync(`/app/.data/db/teams/${parsedJson.id}.json`, JSON.stringify(parsedJson));
     console.log('wrote team info');
   } catch (e) {
     console.log('could not write team info.', e);
