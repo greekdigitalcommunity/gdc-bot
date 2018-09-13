1398676ff0ed5025cb7c50b6df719ba181bd1a52
Thanos Irodotou <iraikky@gmail.com>
2018-09-13 12:18:22 +0100

    Adds ping and jobs route

---
+++ components/routes/jobs.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const fs = require('fs');
+const friendly = process.env.FRIENDLY;
+
+module.exports = function (webserver, controller) {
+    webserver.get('/jobs', (req, res) => {
+        if (req.query) {
+            if (friendly === req.query.key) {
+                let jobData = JSON.parse(fs.readFileSync('.data/db/jobs/jobs.json', 'utf8'));
+                res.json(jobData);
+            }
+        } else {
+            res.status(403);
+        }
+    });
+};
