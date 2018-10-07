ec4c1818b3c3fe187d5b56787ffdd21714fe5e13
thanosIrodotou <iraikky@gmail.com>
2018-10-07 17:24:58 +0100

    wip

--- skills/job.js
+++ skills/job.js
@@ -17,8 +17,19 @@ const syncToLocalFile = (data) =>
   fs.writeFile('.data/db/jobs/jobs.json', JSON.stringify(data), 'utf8', (err) => {
     if (err) {
       console.log('could not sync to local file', err);
+      if (!fs.existsSync('.data/db/jobs')) {
+        console.log('jobs dir does not exist - creating dir and file');
+        fs.mkdirSync('.data/db/jobs');
+        fs.writeFile('.data/db/jobs/jobs.json', JSON.stringify(data), 'utf8', (err) => { 
+            if (err) {
+              console.log('could not resync to local file');
+            } else {
+              console.log('resync was successful');
+            }
+        });
+      }
     } else {
-      console.log('sync to file was successful.');
+      console.log('sync to file was successful');
     }
   });
 const syncToRedis = (data) => redisClient.set(`${JOBS_PREFIX}:storage`, JSON.stringify(data));
